37fa37969df606c71e6df488098acb4c046d75fc
Nikolaos  Goulios <ngoulios@c1r1p9.hive.fi>
2024-09-24 19:28:44 +0300

    initial steps

---
+++ src/fractol.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 18:42:14 by ngoulios          #+#    #+#             */
+/*   Updated: 2024/09/24 19:24:57 by ngoulios         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fractol.h>
+
+int	main(int argc, char **argv)
+{
+	/*I need to access widt hhight so struct it is*/
+	/*Yes a global struct, not a global variable*/
+	t_fractol	f; 
+	
+	if( /*i need a function to check the validity of the arguments*/ )
+		/*print the correct usage, maybe macro at library*/
+	if (/*The window initialization fail then */)
+		/*Do it need a function to initialize the window*/
+		/*We need to exit the program, with a statment cause thats who we are we love statments*/
+	/*initialize the key and scroll hooks*/
+	return (0);
+	
+}
+
+int is_valid(/*some arguments , and maybe the type of fractal*/)
+{
+	/*are all these parameters correct ? Are all these rewuirments met 
+	from the project ? Thaats the function that will check all of those things*/
+	
+}
+
+/*I do need a function to initialize window cause i need to add the correct fractal inside*/
+int	initialize_fractal(t_fractol *f) /*Popably with the struct to manipulate each struct unit*/
+{
+	/*open the window*/
+	/*A lot of checks to int MLX functions
+	  very very cerfull handling cause this part is crusial */
+	/*Edge cases for the initialization ? Yeaaaaaas WHAT IIIIf the user ... you know*/
+	
+	/*Draw the correct fractal, Maybe in a different function*/
+}
+
+/*Yeees, draw fractal function is indeed needed here*/
+void	draw_fractal()
+{
+	
+}
+/*Functions to check the input values for each fractal? 
+Probably cause different fractal needs different inputs and is_valid will be ultra long 
+if I want to check those inside */
+void input()
+{
+	
+}
+
+/*I saw this cool thing that you have a thing next to your window that displays different things 
+whats is it, I want*/
+
+void	cool_display_window()
+{
+	/* I want it and I will find a way to do it, daaa!!! */
+}
---
+++ src/fractol.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 19:17:46 by ngoulios          #+#    #+#             */
+/*   Updated: 2024/09/24 19:27:24 by ngoulios         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+/*what other includes do I need? Maybe the math.h and the classic ones*/
+
+# include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
+# include <MLX42/MLX42.h>
+
+/*The structS needs to be difined here */
+/*What this struct will have inside ? Should I saparate them ?*/
+
+
+/*I need a struct for the imaginary and real number equation*/
+
+/*DO MLX functions needs to be declaired here or ... ? Maybe not*/
+
+/* */
+#endif
